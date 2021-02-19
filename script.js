ff56f0360bb7609f8816637a650ee0084f725597
BraydenLeblanc <brayden.leblanc@ocsbstudent.ca>
2021-02-19 15:45:46 +0000

    fixed js and css errors

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
+// Turns userInput into a variable
 document.getElementById('button').addEventListener('click', comparison)
 let userInput = 0
 
 function comparison () {
-// Get user input
+// Get user input from box
   userInput = document.getElementById('input').value
   
+  // Determines what to say based on the age entered
   if (userInput >= 17) {
     document.getElementById('answer').innerHTML = 'You are able to see an R movie alone, as well as PG-13, PG, and G rated movies.'
-  }  else if (userInput >= 13) {
+  } else if (userInput >= 13) {
     document.getElementById('answer').innerHTML = 'You are able to see an PG-13 movie alone, as well as PG, and G rated movies.'
-  }  else if (userInput >= 5) {
+  } else if (userInput >= 5) {
     document.getElementById('answer').innerHTML = 'You are able to see PG, and G rated movies.'
-  }  else {
+  } else {
     document.getElementById('answer').innerHTML = 'You are too young for most movies'
   }
 }
